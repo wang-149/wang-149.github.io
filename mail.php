ac7835a8a7f8d20705fbb50f71a4a7cfb57553ed
wang-149 <68971762+wang-149@users.noreply.github.com>
2020-07-30 14:29:08 -0400

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,25 @@
+<?php 
+if(isset($_POST['submit'])){
+    $to = "964573140@qq.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $first_name = $_POST['firstname'];
+    $last_name = $_POST['lastname'];
+    $subject = "Form submission";
+//   $subject2 = "Copy of your form submission";
+    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['subject'];
+ //   $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+   // $headers2 = "From:" . $to;
+   if(mail($to,$subject,$message,$headers)){
+        echo "<h1>Sent Successfully! Thank you!!!"." ".$first_name.", We will contact you shortly!</h1>";
+   }
+   else {
+       echo "Something went wrong";
+   }
+  //  mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+   // echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    }
+?>
+
